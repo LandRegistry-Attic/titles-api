77e3ba4f5ee7185413adea8fe14cf95d086e134c
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2015-06-15 20:52:15 +0100

    validate title route

--- application/routes.py
+++ application/routes.py
@@ -2,8 +2,11 @@ from application import app
 
 @app.route('/', methods=["GET"])
 def index():
-    return 'Title API'
+    return 'Titles API'
 
 @app.route('/validate/<titlenumber>', methods=["GET"])
 def validateTitle(titlenumber):
-    return true
+    if titlenumber:
+        return true
+    else:
+        return false
